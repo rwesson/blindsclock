f940bd90f2068cf6e258b533e2aca86d07dde7b3
rwesson <rw@nebulousresearch.org>
2025-05-03 15:30:55 +0100

    replaced Commodores with Rhythm Heritage

--- sounds/make.sh
+++ sounds/make.sh
@@ -4,7 +4,7 @@ dir="/home/roger/music/Compilations/RW's random tracks"
 dir2="/home/roger/music/Public Service Broadcasting/The Race For Space"
 
 ffmpeg -i "$dir"/Chris\ Joss\ -\ Tune\ Down.mp3 -ss 24.5 -t 6.5 clip1.mp3 -y
-ffmpeg -i "$dir"/Commodores\ -\ Machine\ Gun.mp3 -ss 101 -t 4.5 clip2.mp3 -y
+ffmpeg -i "$dir"/Rhythm\ Heritage\ -\ S.W.A.T..mp3 -ss 239.8 -t 6 -af "afade=out:st=244.75:d=1" clip2.mp3 -y
 ffmpeg -i "$dir"/Tomoyasu\ Hotei\ -\ Battle\ Without\ Honour\ or\ Humanity.mp3 -ss 15.75 -t 5.75 clip3.mp3 -y
 ffmpeg -i "$dir"/Faithless\ -\ Insomnia.mp3 -ss 137.8 -t 3.9 clip4.mp3 -y
 ffmpeg -i "$dir"/Daft\ Punk\ -\ Get\ Lucky.mp3 -t 2.35 clip5.mp3 -y
