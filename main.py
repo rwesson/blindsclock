bf925b36b24b2f4765c5ad0e079251c99831cb16
rwesson <rw@nebulousresearch.org>
2025-05-09 18:09:59 +0100

    don't change text colour if it's in the red zone

--- main.py
+++ main.py
@@ -269,12 +269,14 @@ class MainView(StackLayout):
       self.blindsrunning=True
       self.ids.startstop.text="pause"
       self.ids.gametime.color="white"
-      self.ids.timeuntilnextblinds.color="white"
+      if self.ids.timeuntilnextblinds.color=="grey":
+        self.ids.timeuntilnextblinds.color="white"
     else:
       self.blindsrunning=False
       self.ids.startstop.text="resume"
       self.ids.gametime.color="grey"
-      self.ids.timeuntilnextblinds.color="grey"
+      if self.ids.timeuntilnextblinds.color=="white":
+        self.ids.timeuntilnextblinds.color="grey"
 
   def blinds_control(self,opt):
     if opt=="prev":
